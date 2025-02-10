c475a4f50ce0b4cc84d997284a7717f9bade4547
samar-nafea <samarnafie11@gmail.com>
2025-02-10 22:12:54 +0200

    Update Task_3.sql

--- MySQL/Task_3.sql
+++ MySQL/Task_3.sql
@@ -5,4 +5,6 @@ select name from city where  COUNTRYCODE='JPN' ;
 select round(sum(LAT_N),2), round(sum(LONG_W),2)  from STATION ; 
 
 -- Third Problem
+select  distinct city from STATION where not(city like 'A%' or city like 'E%' or city like 'I%' or city like 'O%' or city like 'U%');
 
+-- Fourth Problem
