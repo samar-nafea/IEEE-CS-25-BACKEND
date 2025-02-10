3517e85b222dc5c0f5d33658cf9a49fcc02597d1
samar-nafea <samarnafie11@gmail.com>
2025-02-10 22:15:41 +0200

    Update Task_3.sql

--- MySQL/Task_3.sql
+++ MySQL/Task_3.sql
@@ -8,3 +8,6 @@ select round(sum(LAT_N),2), round(sum(LONG_W),2)  from STATION ;
 select  distinct city from STATION where not(city like 'A%' or city like 'E%' or city like 'I%' or city like 'O%' or city like 'U%');
 
 -- Fourth Problem
+select  distinct city from STATION where not(city like '%A' or city like '%E' or city like '%I' or city like '%O' or city like '%U');
+
+-- Fifth Problem
