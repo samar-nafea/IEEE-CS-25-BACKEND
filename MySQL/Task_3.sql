786eeb94eb953b3dcbd8b9fbfa88b31f0dc4476f
samar-nafea <samarnafie11@gmail.com>
2025-02-12 15:05:56 +0200

    Update Task_3.sql

--- MySQL/Task_3.sql
+++ MySQL/Task_3.sql
@@ -17,7 +17,8 @@ select  distinct city from STATION where not(city like 'A%' or city like 'E%' or
 select floor(avg(population)) from city ;
 
 -- Seventh Problem
-SELECT sender_id, count(message_id)"message_count" FROM messages WHERE sent_date BETWEEn  '2022-08-01 00:00:00' AND '2022-08-31 23:59:59'
-GROUP BY sender_id ORDER BY count(message_id)  DESC LIMIT 2 ;
+SELECT sender_id, count(message_id)"message_count" FROM messages 
+WHERE sent_date BETWEEn  '2022-08-01 00:00:00' AND '2022-08-31 23:59:59'
+GROUP BY sender_id  ORDER BY count(message_id)  DESC LIMIT 2 ;
 
 -- Eighth Problem
