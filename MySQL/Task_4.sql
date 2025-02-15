a7a80b5b6eadf9db78093669e2ba91f01430396a
samar-nafea <samarnafie11@gmail.com>
2025-02-15 19:36:00 +0200

    Update Task_4.sql

--- MySQL/Task_4.sql
+++ MySQL/Task_4.sql
@@ -1,6 +1,13 @@
 /* Problem 1
 Type of Triangle */
-
+select
+case 
+when A + B <= C OR A + C <= B OR B + C <= A then 'Not A Triangle'
+when A=B and B=C then 'Equilateral'
+when A=B or A=C or B=C then 'Isosceles'
+else 'Scalene'
+end 
+from TRIANGLES ;
 
 /* Problem 2
 The PADS */
